88ee424806ae9acf98c416af432da39440991e41
Piash-Akbar <sknafisakbar@gmail.com>
2025-01-16 11:49:50 +0600

    first commit

---
+++ Patterns/AlphabetSquare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+    for( int i=0; i<n; i++){
+        for(char j=65; j<70; j++){
+            cout<< j<<' ';
+        }
+        cout<<endl;
+    }
+}
